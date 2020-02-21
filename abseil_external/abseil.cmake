fd7995d6421d17f1b0afa3bcbbb02003f9a95273
gucchisk <gucchi_sk@yahoo.co.jp>
2020-02-21 09:53:34 +0900

    Refactor

--- abseil_external/abseil.cmake
+++ abseil_external/abseil.cmake
@@ -4,18 +4,9 @@ configure_file(abseil.CMakeLists.txt.in
   abseil-download/CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/abseil-download)
-execute_process(COMMAND ${CMAKE_COMMAND} --build . --target abseil-download
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/abseil-download)
 
-# execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-#   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/abseil-src)
-# execute_process(COMMAND ${CMAKE_COMMAND} --build . --target strings
-#   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/abseil-src)
-
-list(LENGTH ABSEIL_LINK_LIST link_list_length)
-
-if (link_list_length GREATER 0)
-  add_subdirectory(${CMAKE_BINARY_DIR}/abseil-src EXCLUDE_FROM_ALL)
-endif()
-
-include_directories(${CMAKE_BINARY_DIR}/abseil-src)
+add_subdirectory(${CMAKE_BINARY_DIR}/abseil-src
+  ${CMAKE_BINARY_DIR}/abseil-bin
+  EXCLUDE_FROM_ALL)
